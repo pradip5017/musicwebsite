588a75268a28288b1f2f392d088c2e5ee45489f1
pradip5017 <160827582+pradip5017@users.noreply.github.com>
2024-02-22 12:42:34 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Example {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "world";
+
+        String result = str1 + "" + str2;
+        System.out.println(result);
+    }
+}
